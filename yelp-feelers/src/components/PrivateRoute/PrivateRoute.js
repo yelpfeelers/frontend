62242bcba72084481146c0e1182856c904e40c7f
c87 <shaun.carmody@live.co.uk>
2019-03-14 20:52:04 +0000

    remove logger

--- yelp-feelers/src/components/PrivateRoute/PrivateRoute.js
+++ yelp-feelers/src/components/PrivateRoute/PrivateRoute.js
@@ -6,7 +6,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route
         {... rest}
         render={props => (
-            props.isAuth ?
+            localStorage.getItem('token') ?
             <Component {...props} /> :
             <Redirect push to="/account" />
         )}
